dabb2afbf8f7feee6dcc9c306616b9fbcba9941d
Josegomez5 <80526088+Josegomez5@users.noreply.github.com>
2025-03-31 13:04:21 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from modules.auth import login
 from modules.dashboard import mostrar_dashboard
+from modules.estudiantes import gestion_estudiantes
 
 st.set_page_config(layout="wide")
 
@@ -14,6 +15,12 @@ else:
     st.write(f"Has iniciado sesión como: **{st.session_state.rol}**")
 
     if st.session_state.rol == "admin":
-        mostrar_dashboard()
+        menu = ["Dashboard", "Estudiantes"]
+        opcion = st.sidebar.selectbox("Menú", menu)
+
+        if opcion == "Dashboard":
+            mostrar_dashboard()
+        elif opcion == "Estudiantes":
+            gestion_estudiantes()
     else:
         st.info("Módulos aún no disponibles para este rol.")
